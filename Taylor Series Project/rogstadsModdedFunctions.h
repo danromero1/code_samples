8507b8c601b476bd620bc418477c1261f0549fa2
danromero1 <daromero1@cpp.edu>
2024-11-22 00:16:51 -0800

    Add files via upload

---
+++ Taylor Series Project/rogstadsModdedFunctions.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double rogstadsCosExp(double argument, double threshold);
+
+double rogstadsSinExp(double argument, double threshold);
+
+long long rogstadsFact(int argOfFactorial);
+
+double rogstadsTanFunction(double m, double n);
